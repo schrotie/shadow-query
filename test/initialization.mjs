c0d0468b6673b9e626d2c0de1d4ad357b886e7c9
troggendorf <git@schrotie.de>
2018-11-25 13:14:11 +0100

    Changed test to use headless chrome via mocha-headless-chrome runner

--- test/initialization.mjs
+++ test/initialization.mjs
@@ -1,5 +1,4 @@
 import $ from '../shadowQuery.mjs';
-import './testComponent.mjs';
 
 const dom = `\
 <span id="span"></span>\
@@ -13,6 +12,25 @@ const dom = `\
 <p id="p"><label id="label-2"/></p>\
 <test-component id="wc"></test-component>\
 `;
+
+
+const template = document.createElement('template');
+template.innerHTML = `
+	<span id="span"/>
+	<button id="button"/>
+	<div id="div">
+		<label id="label"/>
+	</div>
+	<h1 id="h1-1"/>
+	<h1 id="h1-2"/>
+`;
+window.customElements.define('test-component', class extends HTMLElement {
+	constructor() {super();}
+	connectedCallback() {
+		this.attachShadow({mode:'open'})
+			.appendChild(template.content.cloneNode(true));
+	}
+});
 
 const body = id('test');
 
